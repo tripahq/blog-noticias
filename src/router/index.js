c4a613e628e4bcf79b25d71c11016d830a9acd41
tripahq <nicolas.rodbri@gmail.com>
2026-03-19 23:13:47 -0300

    cambios

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { auth } from '../firebase/config' 
+import { createWebHashHistory, createRouter } from 'vue-router'
+import { auth } from '../firebase/config' //
 
 const routes = [
   { 
@@ -21,26 +21,34 @@ const routes = [
     path: '/create-post', 
     name: 'CreatePost', 
     component: () => import('../views/CreatePostView.vue'),
+    // Agregamos esta meta-información para marcarla como protegida
     meta: { requiresAuth: true } 
   }
 ]
 
 const router = createRouter({
-  // Se cambia el string fijo por la variable de entorno de Vite
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // Se cambia createWebHistory por createWebHashHistory para compatibilidad con GitHub Pages
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
-// Guardia de navegación para proteger rutas
+// Configuramos el "Portero" (Navigation Guard)
 router.beforeEach((to, from, next) => {
+  // Verificamos si la ruta a la que queremos ir (to) tiene la etiqueta requiresAuth
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  
+  // Verificamos en Firebase si hay un usuario logueado actualmente
   const isAuthenticated = auth.currentUser
 
+  // Lógica del portero
   if (requiresAuth && !isAuthenticated) {
+    // Si la ruta requiere autenticación y NO estás logueado, redirige al Login
     next('/login')
   } else if (to.path === '/login' && isAuthenticated) {
+    // Si estás logueado e intentas ir al Login, redirige al Inicio
     next('/')
   } else {
+    // En cualquier otro caso, permite el acceso
     next()
   }
 })
